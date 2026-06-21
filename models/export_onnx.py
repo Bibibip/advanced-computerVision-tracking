b7a8ec4f3a3dba4ea71937889f0a8da7cb776166
SunBi <sunbi2003@gmail.com>
2026-06-21 22:41:30 +0900

    Delete models directory

--- models/export_onnx.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-# 사람 탐지 모델
-base = YOLO('./yolov8n.pt')
-base.export(format='onnx', imgsz=1024, simplify=True, half=True, device=0)
-
-# 물건 탐지 모델
-custom = YOLO('./best.pt')
-custom.export(format='onnx', imgsz=1024, simplify=True, half=True, device=0)
